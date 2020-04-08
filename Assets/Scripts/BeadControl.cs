05ce1cbce5c871dc304251bfa05452daffbaef9d
MMP-CS2450-F19 <59860859+MMP-CS2450-F19@users.noreply.github.com>
2020-04-08 14:52:36 -0600

    Local Project Upload
    Uploading my local project to the repository.

---
+++ Assets/Scripts/BeadControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeadControl : MonoBehaviour
+{
+    //private Rigidbody2D rb2d;
+
+    // bool for bead position, either up or down
+    public bool beadState = false;
+
+    // float for bead movement
+    public float beadMove = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        // if beadState is false, beadState becomes true, bead moves up a value
+        if (!beadState)
+        {
+            beadState = true;
+
+            // move the bead up 1 unit
+            transform.position = new Vector3(transform.position.x, transform.position.y + beadMove, 0.0f);
+
+        }
+        else
+        {
+            beadState = false;
+
+            // move the bead down 1 unit
+            transform.position = new Vector3(transform.position.x, transform.position.y - beadMove, 0.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Lists for beads
+    public List<Sprite> onePlaceList;
+    public List<Sprite> tenPlaceList;
+    public List<Sprite> fivePlaceList;
+    public List<Sprite> fiftyPlaceList;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 
+}
