1c5578df8e3b9936ccd8789b62aaa579879b6058
Cjericho4 <58090256+Cjericho4@users.noreply.github.com>
2020-04-20 16:30:21 -0600

    Code Overhaul
    1) Fixed Bead Movements.
    2) Added Lean Tween (free Unity plug-in for Animations
    3) Changed the way the Controller Script works
    4) Added UI to display Number on Abacus.
    5) Added Text Mesh Pro
    6) Added comments to BeadControl so it can be easily updated
    7)Comments added to Controller.cs for clarity and ease of maintenance.
    8)Added what plugins are used to the README.md and basic explanation of what is used.

--- Assets/Scripts/BeadControl.cs
+++ Assets/Scripts/BeadControl.cs
@@ -1,149 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class BeadControl : MonoBehaviour
 {
-    public Controller controller;
+	//Bead index in the group
+	public int index;
+	//Active or not
+	public bool truthiness;
+	//How much the bead will move on the rail
+	public float moveAmount;
+	//How quickly the animation from LeanTween will be run.
+	public float moveSpeed;
+	//Group of beads. Grouped by value
+	public BeadControl[] beadsGroup;
+	//Beads original position so it can return to its starting position
+	private float homePosition;
 
-    // bead value
-    public int beadVal = 1;
+	private void Start()
+	{
+		//Caches the Beads positions so it knows where it "lives" and can return easily
+		homePosition = transform.localPosition.y;
+		//Bead indexes its own position in the grouping so that it can move everyone above or below them if needed.
+		for (int i = 0; i < beadsGroup.Length; i++)
+		{
+			if (beadsGroup[i].gameObject == this.gameObject)
+			{
+				index = i;
+			}
+		}
+	}
+	//Mouse must be clicked up and down. 
+	private void OnMouseUpAsButton()
+	{
+		ToggleValue();
+		//This will update the count displayed on the screen.
+		FindObjectOfType<Controller>().UpdateCount();
+	}
 
-    // bool for bead position, either up or down
-    public bool beadState = false;
+	public void ToggleValue()
+	{
+		//if true or false
+		if (truthiness)
+		{
+			//DEACTIVATION - DOWN: this loops through the group of beads on the rail and moves them depending on which bead was clicked.
+			for (int i = 0; i < beadsGroup.Length; i++)
+			{
+				if (i >= index)
+				{
+					beadsGroup[i].Activate(false);
+				}
+			}
+		}
+		else
+		{
+			//ACTIVATION - UP: this loops through the group of beads on the rail and moves them depending on which bead was clicked.
+			for (int i = 0; i < beadsGroup.Length; i++)
+			{
+				if (i <= index)
+				{
+					beadsGroup[i].Activate(true);
+				}
+			}
+		}
+	}
 
-    // float for bead movement
-    public float beadMove = 1.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void Move()
-    {
-        // if beadState is false, beadState becomes true, bead moves up a value
-        if (!beadState)
-        {
-            beadState = true;
-
-            // move the bead up 1 unit
-            transform.position = new Vector3(transform.position.x, transform.position.y + beadMove, 0.0f);
-
-        }
-        else
-        {
-            beadState = false;
-
-            // move the bead down 1 unit
-            transform.position = new Vector3(transform.position.x, transform.position.y - beadMove, 0.0f);
-        }
-    }
-
-    void OnMouseDown()
-    {
-        // make a new list to store beads to move
-        List<GameObject> tempList = new List<GameObject>();
-
-        // check to see if the button is up
-        if (!beadState)
-        {
-            // if down - iterate through the list and find the beads that are above the current bead that are also in the down position
-            if(beadVal == 1)
-            {
-                // iterate through the list for value 1 beads
-                int i = 0;
-
-                while(i < 5)
-                {
-                    if (controller.onePlaceList[i].name != this.name && controller.onePlaceList[i].GetComponent<BeadControl>().beadState == false)
-                    {
-                        tempList.Add(controller.onePlaceList[i]);
-                        i++;
-                    }
-                    else
-                    {
-                        // add the bead clicked on, then break
-                        tempList.Add(controller.onePlaceList[i]);
-                        break;
-                    }
-                }
-            }
-            else if (beadVal == 10)
-            {
-                // iterate through the list for value 10 beads
-            }
-            else
-            {
-                //do nothing, more bead rows go here later
-            }
-
-            // move all of those beads up
-            for (int i = 0; i < tempList.Count; i++)
-            {
-                // move the bead
-                tempList[i].transform.position = new Vector3(tempList[i].transform.position.x, tempList[i].transform.position.y + beadMove, 0.0f);
-
-                // set the state of the bead to true
-                tempList[i].GetComponent<BeadControl>().beadState = true;
-            }
-        }
-        // beads are in the up position
-        else
-        {
-            // iterate through all the beads and find the beads that are below the current bead and in the up position
-            if (beadVal == 1)
-            {
-                int beadPosition = 0;
-                // find the position of the clicked bead
-                for (int i  = 0; i < controller.onePlaceList.Count; i++)
-                {
-                    if (controller.onePlaceList[i].name == this.name)
-                    {
-                        // add the clicked bead to the tempList
-                        tempList.Add(controller.onePlaceList[i]);
-                        beadPosition = i;
-                    }
-                }
-                // loop through the remaining beads and check their state
-                for (int j = beadPosition + 1; j < controller.onePlaceList.Count; j++ )
-                {
-                    if(controller.onePlaceList[j].GetComponent<BeadControl>().beadState == true)
-                    {
-                        // add the bead if they are in the up state
-                        tempList.Add(controller.onePlaceList[j]);
-                    }
-                }
-            }
-            else if (beadVal == 10)
-            {
-                // iterate through the list for value 10 beads
-            }
-            else
-            {
-                //do nothing, more bead rows go here later
-            }
-            // move all of those beads down
-            for (int i = 0; i < tempList.Count; i++)
-            {
-                // move the bead down
-                tempList[i].transform.position = new Vector3(tempList[i].transform.position.x, tempList[i].transform.position.y - beadMove, 0.0f);
-
-                // set the state of the bead to false
-                tempList[i].GetComponent<BeadControl>().beadState = false;
-            }
-        }
-
-        // delete the list for garbage collection
-        tempList.Clear();
-
-        // run the function to update the GUI sum
-    }
+	public void Activate(bool activation)
+	{
+		//will set the bool to true or false depending on whether it was true or false before.
+		truthiness = activation;
+		if (activation)
+		{
+			//This will mark the bead as Active (up position for lower beads, down position for upper beads) and WILL be counted.
+			transform.LeanMoveLocalY(homePosition + moveAmount, moveSpeed).setEaseOutElastic();
+		}
+		else
+		{
+			//This will mark the bead as Unactive (down position for the lower beads, up position for higher beads) and wil NOT be counted.
+			transform.LeanMoveLocalY(homePosition, moveSpeed).setEaseOutElastic();
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # M-3_Prototype
 Functional abacus prototype in Unity
+Plug-ins used: LeanTween and Text Mesh Pro (LeanTween is free in the Asset Store and TextMeshPro is bundled with Unity)
+Scripts: BeadControl.cs and Controller.cs
+Scene: Abacus is the Prototype Scene that we used. 
+Prefab: Not used but could be to easily create new scenes. 
